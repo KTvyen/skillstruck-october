adf99ae68c84086bc6a3368cdf2088b2f5ccc8f0
KTvyen <katenguyen08.az@icloud.com>
2024-11-08 08:09:24 -0700

    exported from Skill Struck

---
+++ os_path.py
@@ -0,0 +1,59 @@
+#os path = find, creat, manage, delete, and maintain different folders
+
+#io path = read, write to files
+
+#module = .py files
+
+#package = multiple module
+
+#import = load in other py source files 
+
+#import os
+#os.path.adventure()
+
+
+#import os
+#os.path.isfile()
+#variablename = os.path.isfile("C:\\Users\owl.png")
+#print(variablename)
+
+import os
+#specifies in wheathe the directory exist or not boolen anser TRUE or FALSE
+
+#vn = os.path.isdir("C:\\Users")
+#print(vn)
+
+#checked if the named Users directory existed (it dosen't work in skill struck but it return true)
+
+#os.path.remove() to remove a file i the directory
+
+#assume the file owl exist 
+
+#os.path.remove("C:\\Users\owl.png")
+
+#will remove that file 
+
+#os.path.basename()
+
+#use = returns basename of fie 
+
+
+#vn = os.path.basename("/squirrel/elephant.png")
+#print(vn) #returns elephant.png base conecting with the png
+
+
+#os.path.dirname()
+
+#vn = os.path.dirname("/squirrel/elephant.png")
+#print(vn) #returns name of folder where file is aka squirrel
+
+
+#sys.path()
+#list of directories found in sys.path 
+
+
+
+
+
+
+
