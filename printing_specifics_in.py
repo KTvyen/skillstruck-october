22ed858fccde8313477bc72ac05b09b5f06f8d94
KTvyen <katenguyen08.az@icloud.com>
2024-11-08 08:09:21 -0700

    exported from Skill Struck

---
+++ printing_specifics_in.py
@@ -0,0 +1,12 @@
+#print("I love \n to code")
+#\n new line
+
+#print("I love \t to code!")
+#\t tab
+#print("Today is \r a new day!")
+#\r return after symbol
+
+
+#print("\t\t\t Senior\n\t\t Junior\n\tSophmore\n Freshman")
+print("Once \nupon \na \ntime")
+
