9d1e490e843e3b220a9697b247d5dff71b683a97
KTvyen <katenguyen08.az@icloud.com>
2024-11-08 08:09:19 -0700

    exported from Skill Struck

---
+++ py_math.py
@@ -0,0 +1,50 @@
+import math
+
+#x = math.ceil(3.2)
+#print(x)
+
+#x = math.floor(3.2)
+#print(x)
+
+#x = math.pi 
+#print(x) 
+
+#x = math.sqrt(100)
+#print(x)
+
+#x = pow(2,4)
+#print(x)
+
+#ppl = int(input("number of people"))
+#bike = int(input("number of bikes"))
+#per_bike = math.ceil(ppl/bike)
+#print(f"In this town, for every bike that exists there are {per_bike} people.")
+
+#length = int(input("enter side lenght"))
+
+#print(pow(length,3))
+
+
+
+
+#x = datetime.datetime.now()
+#print(x)
+
+#x = datetime.datetime.now()
+#print(x.strftime("%m"))
+
+#today = datetime.date.today()
+#print(today)
+
+#x = datetime.datetime.now()
+#print(x.strftime("%X"))
+
+#x = datetime.datetime.now()
+#print(x.strftime("%m"))
+
+#x = datetime.datetime.now()
+#print(x.strftime("%d"))
+
+#x = datetime.datetime.now()
+#print(x.strftime("%Y"))
+
