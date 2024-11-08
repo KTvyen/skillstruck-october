79f359a7b20f57be0420917e0c927b67bfd14af7
KTvyen <katenguyen08.az@icloud.com>
2024-11-08 08:09:22 -0700

    exported from Skill Struck

---
+++ Different_Concatenating_Approaches.py
@@ -0,0 +1,27 @@
+# f string appraoch aka f"sentence {format this}"
+
+#comma so like "this is", here goes variable, " continue sentence"
+
+#name = "duke"
+
+#dogs = "8"
+
+#location = "NY"
+
+#print(f"My name is {name} and I have {dogs} dogs and live in {location}")
+#print("My name is", name,"and I have", dogs, "dogs and live in", location)
+
+#print("My name is " + name + " and I have " + dogs + " dogs and live in " + location )
+
+#one = "carrot" two = "cabbage" three = "spinach" four = "onion" five = "beetroot"
+
+#num1 = 1 num2 = 2 num3 = 3 num4 = 4 num5 = 5 
+
+#print(f"{one} {num1} {two} {num2} {three} {num3} {four} {num4} {five} {num5}")
+
+num1 = 1 
+num2 = 2 
+num3 = 3 
+num4 = 4 
+num5 = 5 
+print(num1, num2, num3, num4, num5)
